hsz0707/clang

commit 9ce4627b014808c5dd4c329fa05a50d395ea6c35

Implement target settings for le32-none-ndk triple.
Change-Id: If2f1c6bb9822fdb64c7eb6ef55abc98d9aa3e720

lib/CodeGen/TargetInfo.cpp
@@ -3602,6 +3602,103 @@ llvm::Value *HexagonABIInfo::EmitVAArg(llvm::Value *VAListAddr, QualType Ty,
 }
 
 
+//===----------------------------------------------------------------------===//
+// Android ABI Implementation
+//===----------------------------------------------------------------------===//
+namespace {
+
+class AndroidABIInfo : public ABIInfo {
+public:
+  AndroidABIInfo(CodeGenTypes &CGT) : ABIInfo(CGT) {}
+
+private:
+  ABIArgInfo classifyReturnType(QualType RetTy) const;
+  ABIArgInfo classifyArgumentType(QualType RetTy) const;
+
+  virtual void computeInfo(CGFunctionInfo &FI) const;
+
+  virtual llvm::Value *EmitVAArg(llvm::Value *VAListAddr, QualType Ty,
+                                 CodeGenFunction &CGF) const;
+};
+
+class AndroidTargetCodeGenInfo : public TargetCodeGenInfo {
+public:
+  AndroidTargetCodeGenInfo(CodeGenTypes &CGT)
+    : TargetCodeGenInfo(new AndroidABIInfo(CGT)) {}
+};
+
+}
+
+ABIArgInfo AndroidABIInfo::classifyArgumentType(QualType Ty) const {
+  if (!isAggregateTypeForABI(Ty)) {
+    // Treat an enum type as its underlying type.
+    if (const EnumType *EnumTy = Ty->getAs<EnumType>())
+      Ty = EnumTy->getDecl()->getIntegerType();
+
+    return (Ty->isPromotableIntegerType() ?
+            ABIArgInfo::getExtend() : ABIArgInfo::getDirect());
+  }
+
+  // Ignore empty records.
+  if (isEmptyRecord(getContext(), Ty, true))
+    return ABIArgInfo::getIgnore();
+
+  // Structures with either a non-trivial destructor or a non-trivial
+  // copy constructor are always indirect.
+  if (isRecordWithNonTrivialDestructorOrCopyConstructor(Ty))
+    return ABIArgInfo::getIndirect(0, /*ByVal=*/false);
+
+  // Otherwise, pass by coercing to a structure of the appropriate size.
+  llvm::Type* ElemTy;
+  unsigned SizeRegs;
+  if (getContext().getTypeAlign(Ty) > 32) {
+    ElemTy = llvm::Type::getInt64Ty(getVMContext());
+    SizeRegs = (getContext().getTypeSize(Ty) + 63) / 64;
+  } else {
+    ElemTy = llvm::Type::getInt32Ty(getVMContext());
+    SizeRegs = (getContext().getTypeSize(Ty) + 31) / 32;
+  }
+
+  llvm::Type *STy =
+    llvm::StructType::get(llvm::ArrayType::get(ElemTy, SizeRegs), NULL);
+  return ABIArgInfo::getDirect(STy);
+}
+
+ABIArgInfo AndroidABIInfo::classifyReturnType(QualType RetTy) const {
+  if (RetTy->isVoidType())
+    return ABIArgInfo::getIgnore();
+
+  if (isAggregateTypeForABI(RetTy))
+    return ABIArgInfo::getIndirect(0);
+
+  // Treat an enum type as its underlying type.
+  if (const EnumType *EnumTy = RetTy->getAs<EnumType>())
+    RetTy = EnumTy->getDecl()->getIntegerType();
+
+  return (RetTy->isPromotableIntegerType() ?
+          ABIArgInfo::getExtend() : ABIArgInfo::getDirect());
+}
+
+void AndroidABIInfo::computeInfo(CGFunctionInfo &FI) const {
+    FI.getReturnInfo() = classifyReturnType(FI.getReturnType());
+    for (CGFunctionInfo::arg_iterator it = FI.arg_begin(), ie = FI.arg_end();
+         it != ie; ++it)
+      it->info = classifyArgumentType(it->type);
+}
+
+llvm::Value *AndroidABIInfo::EmitVAArg(llvm::Value *VAListAddr, QualType Ty,
+                                       CodeGenFunction &CGF) const {
+  CGBuilderTy &Builder = CGF.Builder;
+  llvm::Type *PTy =
+    llvm::PointerType::getUnqual(CGF.ConvertType(Ty));
+
+  // Since ABIInfo::EmitVAArg should return a pointer of Ty,
+  // We emit va_arg i8** va_list, Ty* for Ty
+  llvm::Value *v = Builder.CreateVAArg(VAListAddr, PTy);
+  return v;
+}
+
+
 const TargetCodeGenInfo &CodeGenModule::getTargetCodeGenInfo() {
   if (TheTargetCodeGenInfo)
     return *TheTargetCodeGenInfo;
@@ -3694,5 +3791,11 @@ const TargetCodeGenInfo &CodeGenModule::getTargetCodeGenInfo() {
   }
   case llvm::Triple::hexagon:
     return *(TheTargetCodeGenInfo = new HexagonTargetCodeGenInfo(Types));
+
+  case llvm::Triple::le32: {
+    if (Triple.getOS() == llvm::Triple::NDK) {
+      return *(TheTargetCodeGenInfo = new AndroidTargetCodeGenInfo(Types));
+    }
+  }
   }
 }

lib/Basic/Targets.cpp
@@ -3919,6 +3919,78 @@ void PNaClTargetInfo::getGCCRegAliases(const GCCRegAlias *&Aliases,
 } // end anonymous namespace.
 
 
+namespace {
+
+class AndroidNDKTargetInfo : public TargetInfo {
+public:
+  AndroidNDKTargetInfo(const std::string& TripleStr);
+
+  virtual void getTargetDefines(const LangOptions& Opts,
+                                MacroBuilder& Builder) const;
+
+  virtual const char *getVAListDeclaration(bool IsCPlusPlus) const {
+    return IsCPlusPlus ?
+      ("namespace std { struct __va_list { void *__ap; }; }\n"
+       "typedef std::__va_list __builtin_va_list;\n") :
+      ("struct __va_list { void *__ap; };\n"
+       "typedef struct __va_list __builtin_va_list;\n");
+  }
+
+  virtual void getTargetBuiltins(const Builtin::Info*& Records,
+                                 unsigned& NumRecords) const {
+  }
+
+  virtual const char* getClobbers() const {
+    return "";
+  }
+
+  virtual void getGCCRegNames(const char* const*& Names,
+                              unsigned& NumNames) const {
+    Names = NULL;
+    NumNames = 0;
+  }
+
+  virtual void getGCCRegAliases(const TargetInfo::GCCRegAlias*& Aliases,
+                                unsigned& NumAliases) const {
+    Aliases = NULL;
+    NumAliases = 0;
+  }
+
+  virtual bool validateAsmConstraint(const char*& Name,
+                                     TargetInfo::ConstraintInfo& Info) const {
+    return false;
+  }
+};
+
+AndroidNDKTargetInfo::AndroidNDKTargetInfo(const std::string& Triple)
+    : TargetInfo(Triple) {
+  BigEndian = false;
+  NoAsmVariants = true;
+
+  DoubleAlign = LongLongAlign = LongDoubleAlign = SuitableAlign = 64;
+
+  SizeType = UnsignedInt;
+  PtrDiffType = SignedInt;
+  IntPtrType = SignedInt;
+
+  MaxAtomicPromoteWidth = MaxAtomicInlineWidth = 64;
+  RegParmMax = 3;
+  CXXABI = clang::CXXABI_ARM;
+  UseZeroLengthBitfieldAlignment = true;
+
+  DescriptionString = "e-p:32:32:32-i1:8:8-i8:8:8-i16:16:16-i32:32:32-"
+                      "i64:32:64-f32:32:32-f64:32:64-v64:64:64-"
+                      "v128:64:128-a0:0:64-n32-S64";
+}
+
+void AndroidNDKTargetInfo::getTargetDefines(const LangOptions& Opts,
+                                         MacroBuilder& Builder) const {
+  Builder.defineMacro("__ANDROID__");
+  Builder.defineMacro("__ELF__");
+}
+
+} // end anonymous namespace
+
 //===----------------------------------------------------------------------===//
 // Driver code
 //===----------------------------------------------------------------------===//
@@ -4015,6 +4087,8 @@ static TargetInfo *AllocateTarget(const std::string &T) {
     switch (os) {
       case llvm::Triple::NativeClient:
         return new PNaClTargetInfo(T);
+      case llvm::Triple::NDK:
+        return new AndroidNDKTargetInfo(T);
       default:
         return NULL;
     }

commit 49c13b39caae34e61509823707392b9859bf77c9

Fix clang le32 driver regression.
This commit fix the regression since 9ce4627 due
to le32-none-nativeclient no longer go to the
default case.

Change-Id: I64bf73fb32e206f78b407f9e7dc36cafd4c2a841

lib/CodeGen/TargetInfo.cpp
@@ -3795,6 +3795,8 @@ const TargetCodeGenInfo &CodeGenModule::getTargetCodeGenInfo() {
   case llvm::Triple::le32: {
     if (Triple.getOS() == llvm::Triple::NDK) {
       return *(TheTargetCodeGenInfo = new AndroidTargetCodeGenInfo(Types));
+    } else {
+      return *(TheTargetCodeGenInfo = new DefaultTargetCodeGenInfo(Types));
     }
   }
   }