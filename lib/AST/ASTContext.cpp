97d4da6421928ab1cf5297eca9393cce5a514524
WenHan Gu <Wenhan.gu@mediatek.com>
2014-07-28 17:29:22 +0800

    [ndk][pndk] Typedef of va_List should be implicit.

--- lib/AST/ASTContext.cpp
+++ lib/AST/ASTContext.cpp
@@ -5872,6 +5872,7 @@ CreateNDK64ABIBuiltinVaListDecl(const ASTContext *Context) {
                                /*Inline*/false, SourceLocation(),
                                SourceLocation(), &Context->Idents.get("std"),
                                /*PrevDecl*/0);
+    NS->setImplicit();
 
     VaListTagDecl = CXXRecordDecl::Create(*Context, TTK_Struct,
                                           Context->getTranslationUnitDecl(),
@@ -5950,6 +5951,7 @@ CreateNDK64ABIBuiltinVaListDecl(const ASTContext *Context) {
                           SourceLocation(), SourceLocation(),
                           &Context->Idents.get("__builtin_va_list"),
                           TInfo);
+  VaListTypedefDecl->setImplicit();
 
   return VaListTypedefDecl;
 }
